61109faae5487d9fc2d210920bde3aad6f7dcf78
Kshitij <kumar.kshitij@siemens.com>
2020-03-18 13:39:44 +0530

    Updating Latest version.

---
+++ AuthServerWithUI/AuthWithUI/AuthWithUI/Configuration/InMemoryConfiguration.cs
@@ -0,0 +1,120 @@
+﻿using IdentityServer4;
+using IdentityServer4.Models;
+using IdentityServer4.Test;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace SocialNetwork.OAuth.Configuration
+{
+    public class InMemoryConfiguration
+    {
+        public static IEnumerable<ApiResource> ApiResources()
+        {
+            return new[] {
+                new ApiResource("authserver", "Auth Server")
+                {
+                    UserClaims=new[]{ "email"}
+                }
+            };
+        }
+
+        public static IEnumerable<IdentityResource> IdentityResources()
+        {
+            return new IdentityResource[] {
+               new IdentityResources.OpenId(),
+               new IdentityResources.Profile(),
+               new IdentityResources.Email()
+            };
+        }
+
+        public static IEnumerable<Client> Clients()
+        {
+            return new[] {
+                new Client
+                {
+                    ClientId = "authserver",
+                    ClientSecrets = new [] { new Secret("secret".Sha256()) },
+                    AllowedGrantTypes = GrantTypes.ResourceOwnerPasswordAndClientCredentials,
+                     AllowedScopes = new [] { "authserver" }
+                },
+                  new Client
+                {
+                    ClientId = "Auth_Implicit",
+                    ClientSecrets = new [] { new Secret("secret".Sha256()) },
+                    AllowedGrantTypes = GrantTypes.Implicit,
+                    AllowedScopes = new [] {
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile,
+                        "authserver"
+                    },
+                    AllowAccessTokensViaBrowser = true,
+                    RedirectUris = new [] { "http://localhost:3068" },
+                  //  PostLogoutRedirectUris = { "http://localhost:5000/signout-callback-oidc" },
+                },
+                //new Client
+                //{
+                //    ClientId = "Auth_Implicit",
+                //    ClientSecrets = new [] { new Secret("secret".Sha256()) },
+                //    AllowedGrantTypes = GrantTypes.Implicit,
+
+                //     AllowedScopes = new [] {
+                //        IdentityServerConstants.StandardScopes.OpenId,
+                //        IdentityServerConstants.StandardScopes.Profile,
+                //        "authserver"
+
+                //    },
+                //     AllowAccessTokensViaBrowser=true,
+                //   AllowedCorsOrigins = { "http://132.186.195.49:4200", "https://132.186.195.49:8080", "http://localhost:4200",
+                //        "https://localhost:4200","https://132.186.195.70:5000","http://132.186.195.70:5000" },
+
+                //    //RedirectUris=new[]{ "http://132.186.195.70:3068/signin-oidc" }
+                //    RedirectUris=new[]{ "http://localhost:3068/signin-oidc" },
+                //     PostLogoutRedirectUris={ "http://localhost:3068/signout-callback-oidc" }
+                //    //PostLogoutRedirectUris={ "http://l132.186.195.70:3068/signout-callback-oidc" }
+                //},
+                 new Client
+                {
+                    ClientId = "Auth_code",
+                    ClientSecrets = new [] { new Secret("secret".Sha256()) },
+                    AllowedGrantTypes = GrantTypes.Hybrid,
+
+                     AllowedScopes = new [] {
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile,
+                        IdentityServerConstants.StandardScopes.Email,
+                        "authserver"
+
+                    },
+                     AllowOfflineAccess=true,
+                     AllowAccessTokensViaBrowser=true,
+                    RedirectUris=new[]{ "http://localhost:3068/signin-oidc" },
+                    PostLogoutRedirectUris={ "http://localhost:3068/signout-callback-oidc"}
+                }
+
+            };
+        }
+
+        public static IEnumerable<TestUser> Users()
+        {
+            return new[] {
+                new TestUser
+                {
+                    SubjectId = "1",
+                    Username = "kshitij",
+                    Password = "password",
+                    Claims=new[]{ new Claim("email","kshitij@gmail.com") }
+                },
+                 new TestUser
+                {
+                    SubjectId = "2",
+                    Username = "admin",
+                    Password = "admin",
+                     Claims=new[]{ new Claim("email","kshitij@gmail.com") }
+                }
+            };
+        }
+    }
+}
---
+++ AuthServerWithUI/AuthWithUI/AuthWithUI/Startup.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Cors.Internal;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using SocialNetwork.OAuth.Configuration;
+
+namespace AuthWithUI
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddIdentityServer()
+               //.AddSigningCredential(new X509Certificate2(@"C:\Program Files\OpenSSL-Win64\bin\auth.pfx", ""))
+               .AddSigningCredential(new X509Certificate2(@"auth.pfx", ""))
+               .AddTestUsers(InMemoryConfiguration.Users().ToList())
+               .AddInMemoryClients(InMemoryConfiguration.Clients())
+               .AddInMemoryIdentityResources(InMemoryConfiguration.IdentityResources())
+               .AddInMemoryApiResources(InMemoryConfiguration.ApiResources());
+            services.AddCors(m => m.AddPolicy("localhost", o =>
+           o.WithOrigins("http://132.186.195.49:4200", "http://localhost:4200", "https://localhost:4200", "http://132.186.195.70:5000",
+           "https://132.186.195.70:5000")
+           .AllowAnyMethod() // mising part
+           .AllowAnyHeader() // mising part
+           ));
+            services.AddMvc();
+           
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole();
+         
+            app.UseDeveloperExceptionPage();
+
+            app.UseIdentityServer();
+           
+            app.UseStaticFiles();
+            
+            //app.UseCors("AllowMyOrigin");
+            app.UseMvcWithDefaultRoute();
+        }
+    }
+}
